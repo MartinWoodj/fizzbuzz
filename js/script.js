3b3a472bee43ae20e3f5484d04d549a3851b2aff
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-05-21 02:22:17 -0400

    FizzBuzz now works and prints to webpage

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@ $(document).ready(function(){
 	for (var i=1; i<=100; i++) {
 		if (i % 3 === 0 && i % 5 === 0) {
 			console.log("FizzBuzz");
+			$('#list').append('<li>'+"FizzBuzz"+'</li>');
 		}
 		else if (i % 3 === 0) {
 			console.log("Fizz");
+			$('#list').append('<li>'+"Fizz"+'</li>');
 		}
 		else if (i % 5 === 0) {
 			console.log("Buzz");
+			$('#list').append('<li>'+"Buzz"+'</li>');
 		}
 		else {
 			console.log(i);
+			$('#list').append('<li>'+i+'</li>');
 		}
 	}
 	
